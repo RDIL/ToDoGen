30f2625e5ac403487d36e9dad6ec48ac15e8bca7
RDIL <34555510+RDIL@users.noreply.github.com>
2018-08-22 09:32:25 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ToDoGen',
+    version='v1.0.0',
+    packages=[''],
+    url='https://github.com/RDIL/ToDoGen',
+    license='MIT License',
+    author='RDIL',
+    author_email='contactspaceboom@gmail.com',
+    description='A To-do list generator, written in Python!'
+)
