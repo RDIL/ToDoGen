f980b979b6277041c4e631ab1a3c2e7a7a0e6fa8
RDIL <34555510+RDIL@users.noreply.github.com>
2018-09-11 21:17:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@
 import os.path
 import sys
 
-print("Welcome to the To-do gen.  How this is going to work is there are going to be 20 slots, and you have to fill")
-print("each one with something.  Once you put something in them, press enter.  You may leave some blank.  ")
-print("")
-
 maximumInputSize = input("TODO list size? ")
 currentInputIndex = 0
 
